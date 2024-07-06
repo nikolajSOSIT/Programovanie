24fba329d1326a930a680ece6f0cdf08b4165bef
NikolajSOSIT <nikolaj.kalinin@student.ssnd.sk>
2024-07-06 16:18:29 +0200

    small changes

---
+++ C++/Tetris.cpp
@@ -0,0 +1,8 @@
+#include <raylib.h>
+
+int main() {
+    InitWindow(300, 600, "raylib Tetris");
+
+    CloseWindow();
+    return 0;
+}
---
+++ Python/InputSk/5Podprogramy/3priemer.py
@@ -0,0 +1,11 @@
+"""
+Napíš funkciu priemer(a, b), ktorá vypočíta priemer dvoch zadaných čísel. 
+Funkcia nič nevypisuje, ale pomocou return vráti vypočítanú hodnotu. 
+Otestuj ju s rôznymi hodnotami parametrov. Napríklad:
+"""
+
+def priemer(a, b):
+    return (a + b) / 2
+
+print(priemer(1, 4))
+print(priemer(3.14, 31.4))
