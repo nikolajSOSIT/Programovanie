35c0c2133591b17c6e7f9ecbf44c9360c575917e
NikolajSOSIT <nikolaj.kalinin@student.ssnd.sk>
2024-07-08 13:53:39 +0200

    C++test

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,25 +1,56 @@
-#include "raylib.h"
-#include <string>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main() {
-	const int width = 300;
-	const int height = 600;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
-	InitWindow(width, height, "Hello");
 
-	while (!WindowShouldClose()){
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		int x = 0;
-		int y = 0;
-		for (int i = 0; i < 51; i++)
-		{
-			unsigned char r = i * 5;
-			Color color = {r,r, 0, 255};
-			DrawRectangle(x, y, x+5, 300, color);	
-			x += 5;
-		}
-		EndDrawing();
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
 	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return 0;
 }
