08fb8b03675038f0044d5e54170414e53a89a953
NikolajSOSIT <nikolaj.kalinin@student.ssnd.sk>
2024-07-11 12:43:22 +0200

    Vulkan Triangle

---
+++ W3Schools/C++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <string> 
+using namespace std; 
+ 
+int main() 
+{ 
+ string cars[3] = {"bmw", "rav4", "volvo"}; 
+ for(int a = 0; a < 3; a++) 
+ { 
+    cout << cars[a] << "\n"; 
+ } 
+ return 0; 
+}
