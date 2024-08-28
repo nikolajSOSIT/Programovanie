a317c00e196c9fbe3ad203168eefc7ff828d65fa
NikolajSOSIT <nikolaj.kalinin@student.ssnd.sk>
2024-08-28 14:28:04 +0200

    adding new lib

--- Python/main.py
+++ Python/main.py
@@ -1,59 +1,31 @@
-import pygame
-
-from pygame.locals import *
-
-from OpenGL.GL import *
-from OpenGL.GLU import *
-
-verticies = (
-   (1, -1, -1),
-   (1, 1, -1),
-   (-1, 1, -1),
-   (-1, -1, -1),
-   (1, -1, 1),
-   (1, 1, 1),
-   (-1, -1, 1),
-   (-1, 1, 1)
-)
-edges = (
-   (0,1),
-   (0,3),
-   (0,4),
-   (2,1),
-   (2,3),
-   (2,7),
-   (6,3),
-   (6,4),
-   (6,7),
-   (5,1),
-   (5,4),
-   (5,7)
-)
-def Cube():
-   glBegin(GL_LINES)
-   for edge in edges:
-      for vertex in edge:
-         glVertex3fv(verticies[vertex])
-   glEnd()
-
-def main():
-   pygame.init()
-   display = (800,600)
-   pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-
-   gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-
-   glTranslatef(0.0,0.0, -5)
-
-   while True:
-      for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-      glRotatef(1, 3, 1, 1)
-      glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-      Cube()
-      pygame.display.flip()
-      pygame.time.wait(10)
-
-main()
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)
