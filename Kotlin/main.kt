d5bf94c9210a7aa98f04c82490f80a6db62e27d5
NikolajSOSIT <nikolaj.kalinin@student.ssnd.sk>
2024-09-13 20:00:49 +0200

    testing some basic stuff

--- Kotlin/main.kt
+++ Kotlin/main.kt
@@ -1,3 +1,16 @@
+class Car(var brand: String, var model: String, var year: Int) {
+  // Class function
+  fun drive() {
+    println("Wrooom!")
+  }
+}
+
 fun main() {
-    println("Hello World!")
+  val c1 = Car("Ford", "Mustang", 1969)
+  
+  // Print property values
+  println(c1.brand + " " + c1.model + " " + c1.year)
+  
+  // Call the function
+  c1.drive()  
 }
---
+++ Switft/swift/Package.swift
@@ -0,0 +1,1 @@
+print("The current value of friendlyWelcome is")
