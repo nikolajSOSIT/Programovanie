83d03c02e9998036b5e71e388427f56e1227e7ee
NikolajDev <nikolajkalinin2006@gmail.com>
2024-09-21 21:27:26 +0200

    changing basic

--- Python/3Dobj.py
+++ Python/3Dobj.py
@@ -1,3 +1,21 @@
-import pygame as pg
 from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
 
+def draw():
+    xrot = 45
+    yrot = 45
+    glClear(GL_COLOR_BUFFER_BIT)
+    glRotatef(xrot, 1.0, 0.0, 0.0)
+    glRotatef(yrot, 0.0, 1.0, 0.0)
+    glutWireCube(0.7)
+    glFlush()
+
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(100, 100)
+glutCreateWindow("Cube")
+glutDisplayFunc(draw)
+glutMainLoop()
