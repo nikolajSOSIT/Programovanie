f18c724e2fe82ff8ada4e3ae7a985e56652931ab
NikolajDev <nikolajkalinin2006@gmail.com>
2024-10-12 19:34:15 +0200

    finally working vulkan library

---
+++ JS/main.js
@@ -0,0 +1,3 @@
+// Start by declaring a rangeRegex variable and assign it a regular expression that matches A through J (the range of columns in your spreadsheet). Use a capture group with a character class to achieve this.
+
+const rangeRegex = /[A-J]/;
